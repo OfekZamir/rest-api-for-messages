6ea88a005bef14bdc9bc186810cbc5a0f1786961
OfekZamir <zamirofek132@gmail.com>
2021-11-16 20:45:51 +0200

    finish

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 //Import Routes
 const messagesRoute = require("./routes/messages");
+const { application } = require("express");
 
 app.use("/messages", messagesRoute);
 
