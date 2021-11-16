b7df708357055da29b93bc120e7f51d2a72a0ebd
OfekZamir <zamirofek132@gmail.com>
2021-11-16 22:07:33 +0200

    adding info section to "/"

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ const messagesRoute = require("./routes/messages");
 app.use("/messages", messagesRoute);
 
 app.get("/", (req, res) => {
-  res.send("Commands api:");
+  res.send(
+    'site = https://message-handler-test.herokuapp.com\
+  MESSAGE contains:\
+      "sender" : String,\
+      "receiver": String,\
+      "subject": String,\
+      "message": String\
+  api commands:\
+  1.Write message in general -> POST site/messages MESSAGE \
+  2.Write message from user to user ->  POST site/messages/USER MESSAGE(without sender)\
+  3.Get all messages for a specific user -> GET site/messages/USER\
+  4.Get all unread messages for a specific user -> GET site/messages/USER/unread\
+  5.Read message (return one message) - GET site/messages/USER/MESSAGE.id\
+  6.Delete message (as owner or as receiver) - DELETE site/messages/USER/MESSAGE.id'
+  );
 });
 
 app.listen(process.env.PORT || 5000);
